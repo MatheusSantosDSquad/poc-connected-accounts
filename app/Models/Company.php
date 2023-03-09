5f8ea5cdc19cee291dcb0de451bf143a831709b3
Matheus Santos <matheussantos@devsquad.com>
2023-03-09 13:43:47 -0300

    Added factory for company model

--- app/Models/Company.php
+++ app/Models/Company.php
@@ -2,12 +2,14 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Laravel\Cashier\Billable;
 
 class Company extends Model
 {
     use Billable;
+    use HasFactory;
 
     protected $fillable = [
         'name'
