865feb6b89d892792d6833a8c7e2a3410496d790
Matheus Santos <matheussantos@devsquad.com>
2023-03-10 09:00:52 -0300

    Added tests for subscription

--- tests/Feature/SubscriptionController/subscribe.php
+++ tests/Feature/SubscriptionController/subscribe.php
@@ -1,18 +1,33 @@
 <?php
 
 use App\Models\Company;
+use App\Models\Plan;
+use Laravel\Cashier\Subscription;
+use Laravel\Cashier\SubscriptionItem;
+use function Pest\Laravel\assertDatabaseHas;
 use function Pest\Laravel\postJson;
 use function PHPUnit\Framework\assertTrue;
 
 it('should attach a new payment method on customer', function () {
     $company = Company::first();
+    $plan    = Plan::first();
 
-    postJson(route('subscription.subscribe'), ['payment_method' => 'pm_1MjowVBsO6X9dLQ6cs8T4siH']);
+    postJson(route('subscription.subscribe'), ['payment_method' => 'pm_1MjowVBsO6X9dLQ6cs8T4siH'])
+        ->assertRedirectToRoute('subscription.index')
+        ->assertSee('You have been subscribed successfully');
 
     $company->refresh();
 
     assertTrue($company->hasPaymentMethod());
     assertTrue($company->hasDefaultPaymentMethod());
 
-    dd($company->defaultPaymentMethod());
+    assertDatabaseHas(Subscription::class, [
+        'name'         => $plan->name,
+        'stripe_price' => $plan->stripe_price,
+    ]);
+
+    assertDatabaseHas(SubscriptionItem::class, [
+        'stripe_product' => $plan->stripe_product,
+        'stripe_price'   => $plan->stripe_price,
+    ]);
 });
