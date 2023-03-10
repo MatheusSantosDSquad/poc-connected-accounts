1528c7ce52ba1a4fc55176103899520066c6b8e3
Matheus Santos <matheussantos@devsquad.com>
2023-03-10 11:40:39 -0300

    Added option that creates a new payment intent for the user

--- app/Http/Controllers/CheckoutController.php
+++ app/Http/Controllers/CheckoutController.php
@@ -2,12 +2,27 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
+use App\Models\Company;
+use App\Models\User;
 
 class CheckoutController extends Controller
 {
     public function index()
     {
+        $company = Company::latest()->first();
+        $user    = User::latest()->first();
 
+        $payment = $user->payWith('', ['card'], [
+            'payment_intent_data' => [
+                'application_fee_amount' => 2, // O valor que vamos transferir para o marketplace, nesse caso, a glossy finis
+                'transfer_data'          => [
+                    'destination' => $company->stripe_account,
+                ],
+            ],
+        ]);
+
+        return view('checkout.index', [
+            'intent' => $payment->clientSecret(),
+        ]);
     }
 }
