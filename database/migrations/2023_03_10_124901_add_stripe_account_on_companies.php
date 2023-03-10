45f7858b09a6a0e9a4b76299ceb9b60ee503bbd3
Matheus Santos <matheussantos@devsquad.com>
2023-03-10 10:29:35 -0300

    Added `stripe_account` field on `companies`

---
+++ database/migrations/2023_03_10_124901_add_stripe_account_on_companies.php
@@ -0,0 +1,26 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    public function up(): void
+    {
+        Schema::table(
+            'companies',
+            fn (Blueprint $table) => $table->string('stripe_account')
+                ->nullable()
+                ->index()
+                ->after('trial_ends_at')
+        );
+    }
+
+    public function down(): void
+    {
+        Schema::table(
+            'companies', fn (Blueprint $table) => $table->dropColumn('stripe_account')
+        );
+    }
+};
