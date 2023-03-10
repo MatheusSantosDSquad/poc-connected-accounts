aad44078f3fec65b9b19afff9c8edb605fb23dc0
Matheus Santos <matheussantos@devsquad.com>
2023-03-10 13:32:22 -0300

    Added option that passes the fee amount to main account

--- app/Http/Controllers/CheckoutController.php
+++ app/Http/Controllers/CheckoutController.php
@@ -2,8 +2,7 @@
 
 namespace App\Http\Controllers;
 
-use App\Models\Company;
-use App\Models\User;
+use App\Models\{Company, User};
 
 class CheckoutController extends Controller
 {
@@ -12,9 +11,13 @@ class CheckoutController extends Controller
         $company = Company::latest()->first();
         $user    = User::latest()->first();
 
-        $payment = $user->payWith('', ['card'], [
+        // 100 dolares
+        $amount = 10000;
+
+        $payment = $user->payWith($amount, ['card'], [
             'payment_intent_data' => [
-                'application_fee_amount' => 2, // O valor que vamos transferir para o marketplace, nesse caso, a glossy finis
+                // O valor que vamos transferir para o marketplace, nesse caso, a glossy finish
+                'application_fee_amount' => $company->calculateFeeAmount($amount),
                 'transfer_data'          => [
                     'destination' => $company->stripe_account,
                 ],
